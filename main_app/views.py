989252bc6c8bf24b4bc05daaeae2f91e50e68a05
TheForeverOptimist <sean@elevate.app>
2023-04-12 07:42:48 -0400

    checking updates

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+#baby step usually a model willbe used
+dogs = [
+    {'name': 'Lolo', 'breed': 'spaniel', 'description': 'cutest', 'age': 3},
+    {'name': 'Cash', 'breed': 'shitzu', 'description': 'fiesty', 'age': 1},
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def index(request):
+    return render(request, 'index.html', {
+        'dogs': dogs
+    })
