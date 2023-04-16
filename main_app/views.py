74080caee654561e4b42930da126ad3b27c116a3
TheForeverOptimist <sean@elevate.app>
2023-04-16 18:38:54 -0400

    adjusted models with class based views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Dog
+from django.views.generic import ListView
 
 def home(request):
     return render(request, 'home.html')
@@ -15,3 +17,11 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', {'dog': dog})
+
+class DogList(ListView):
+    model = Dog
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs'
