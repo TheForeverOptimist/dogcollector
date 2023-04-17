d50935f080d8fe0f1525ad356c45e529b6950c9c
TheForeverOptimist <sean@elevate.app>
2023-04-16 20:27:06 -0400

    class based views model updates

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 from django.views.generic import ListView
 
@@ -24,4 +24,11 @@ class DogList(ListView):
 class DogCreate(CreateView):
     model = Dog
     fields = '__all__'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+    model = Dog
     success_url = '/dogs'
