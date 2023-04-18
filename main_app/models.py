067c2776d7ae936d5b43284f9165b2ead8f33ad6
TheForeverOptimist <sean@elevate.app>
2023-04-17 21:51:20 -0400

    added toys photos

--- main_app/models.py
+++ main_app/models.py
@@ -26,7 +26,7 @@ class Dog(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Cat, Toy, Photo
+from .models import Dog, Toy, Photo
 from .forms import FeedingForm
 
 # def home(request):
